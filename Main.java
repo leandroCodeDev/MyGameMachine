b1f31de02596f03c100a2fd6cbc070f7da55c7f0
lvd <lvd@certi.org.br>
2024-03-04 19:12:10 -0300

    adicionado rtequisito para "Iniciar o Jogo"

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Seja bem vindo ao jogo \"Par\" ou \"Impar\" \n");
+        System.out.println("Instruções: Você escolherá ser Par ou Ímpar e em seguida escolherá um número de 0 a 10.\n" +
+                "O computador também escolherá um número de 0 a 10. Se a soma dos dois números for par e você escolheu Par, você ganha! E vice-versa.");
+
+
+    }
+}
